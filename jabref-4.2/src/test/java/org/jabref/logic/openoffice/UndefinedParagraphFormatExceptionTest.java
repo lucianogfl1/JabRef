a73243d9984eb7b39f3fcfc2be850243e05af0a3
lucianogfl1 <lucianogfl1@gmail.com>
2018-06-21 21:10:26 -0300

    criacao da classe de teste UndefinedParagraphFormatExceptionTest.java

---
+++ jabref-4.2/src/test/java/org/jabref/logic/openoffice/UndefinedParagraphFormatExceptionTest.java
@@ -0,0 +1,32 @@
+package org.jabref.logic.openoffice;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UndefinedParagraphFormatExceptionTest {
+	
+	@Test
+	public void testUndefinedParagraphFormatExceptionFormatName() {
+		UndefinedParagraphFormatException fn = new UndefinedParagraphFormatException("formatname");
+		assertEquals("formatname", fn.getFormatName());
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
