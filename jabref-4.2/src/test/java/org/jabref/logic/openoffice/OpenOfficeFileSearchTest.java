ac25af76321a86be6e99ff945214a779a208fa09
lucianogfl1 <lucianogfl1@gmail.com>
2018-07-10 07:22:53 -0300

    criacao da classe de teste OpenOfficeFileSearchTest.java

---
+++ jabref-4.2/src/test/java/org/jabref/logic/openoffice/OpenOfficeFileSearchTest.java
@@ -0,0 +1,56 @@
+package org.jabref.logic.openoffice;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.Assert.assertNull;
+
+
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class OpenOfficeFileSearchTest {
+
+	@Test
+	public void testdetectInstallationsNotNull() {
+		assertNotNull(OpenOfficeFileSearch.detectInstallations());
+		
+	}
+	
+	@Test
+	public void testfindOpenOfficeDirectoriesNotNull() {
+		
+		//OpenOfficeFileSearch op = new OpenOfficeFileSearch();
+		//op.findOpenOfficeDirectories(null);
+		List<Path> caminho = OpenOfficeFileSearch.detectInstallations();
+		  assertNotNull(OpenOfficeFileSearch.findOpenOfficeDirectories(caminho));
+		  
+	}
+	
+	@Test
+	public void testfindWindowsOpenOfficeDirsNotNull() {
+		assertNotNull(OpenOfficeFileSearch.findWindowsOpenOfficeDirs());
+		
+	}
+	
+	@Test
+	public void testfindOSXOpenOfficeDirsNotNull() {
+		assertNotNull(OpenOfficeFileSearch.findOSXOpenOfficeDirs());
+		
+	}
+	
+	@Test
+	public void testfindLinuxOpenOfficeDirsNotNull() {
+		assertNotNull(OpenOfficeFileSearch.findLinuxOpenOfficeDirs());
+		
+	}
+	
+	
+	
+
+	
+	
+
+}
