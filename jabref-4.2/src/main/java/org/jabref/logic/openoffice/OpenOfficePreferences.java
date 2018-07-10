50ebc979f1d0d8d39712c29296543e8e188e2b15
lucianogfl1 <lucianogfl1@gmail.com>
2018-07-10 07:19:47 -0300

    pequenas alterações na calsse

--- jabref-4.2/src/main/java/org/jabref/logic/openoffice/OpenOfficePreferences.java
+++ jabref-4.2/src/main/java/org/jabref/logic/openoffice/OpenOfficePreferences.java
@@ -28,7 +28,7 @@ public class OpenOfficePreferences {
     private String currentStyle;
     private String jarsPath;
 
-    public OpenOfficePreferences(
+    public OpenOfficePreferences(//testado 
             String jarsPath,
             String executablePath,
             String installationPath,
@@ -48,7 +48,7 @@ public class OpenOfficePreferences {
         this.currentStyle = currentStyle;
     }
 
-    public void clearCurrentStyle() {
+    public void clearCurrentStyle() {//testado
         this.currentStyle = null;
         // TODO: sync to prefs
     }
@@ -56,73 +56,73 @@ public class OpenOfficePreferences {
     /**
      * path to soffice-file
      */
-    public String getExecutablePath() {
+    public String getExecutablePath() {//testado 
         return executablePath;
     }
 
-    public void setExecutablePath(String executablePath) {
+    public void setExecutablePath(String executablePath) {//testado
         this.executablePath = executablePath;
     }
 
     /**
      * main directory for OO/LO installation, used to detect location on Win/OS X when using manual connect
      */
-    public String getInstallationPath() {
+    public String getInstallationPath() {//testado 
         return installationPath;
     }
 
-    public void setInstallationPath(String installationPath) {
+    public void setInstallationPath(String installationPath) {//testado
         this.installationPath = installationPath;
     }
 
     /**
      * true if all databases should be used when citing
      */
-    public Boolean getUseAllDatabases() {
+    public Boolean getUseAllDatabases() {//testado
         return useAllDatabases;
     }
 
-    public void setUseAllDatabases(Boolean useAllDatabases) {
+    public void setUseAllDatabases(Boolean useAllDatabases) {//testado
         this.useAllDatabases = useAllDatabases;
     }
 
     /**
      * true if the reference list is updated when adding a new citation
      */
-    public Boolean getSyncWhenCiting() {
+    public Boolean getSyncWhenCiting() {//testado 
         return syncWhenCiting;
     }
 
-    public void setSyncWhenCiting(Boolean syncWhenCiting) {
+    public void setSyncWhenCiting(Boolean syncWhenCiting) {//testado 
         this.syncWhenCiting = syncWhenCiting;
     }
 
     /**
      * true if the OO panel is shown on startup
      */
-    public Boolean getShowPanel() {
+    public Boolean getShowPanel() {//testado 
         return showPanel;
     }
 
-    public void setShowPanel(Boolean showPanel) {
+    public void setShowPanel(Boolean showPanel) {//testado
         this.showPanel = showPanel;
     }
 
     /**
      * list with paths to external style files
      */
-    public List<String> getExternalStyles() {
+    public List<String> getExternalStyles() {//testado
         return externalStyles;
     }
 
-    public void setExternalStyles(List<String> externalStyles) {
+    public void setExternalStyles(List<String> externalStyles) {//testado 
         this.externalStyles = externalStyles;
     }
 
     /**
      * path to the used style file
      */
-    public String getCurrentStyle() {
+    public String getCurrentStyle() { //testado 
         return currentStyle;
     }
 
@@ -133,21 +133,21 @@ public class OpenOfficePreferences {
     /**
      * directory that contains juh.jar, jurt.jar, ridl.jar, unoil.jar
      */
-    public String getJarsPath() {
+    public String getJarsPath() {//testado 
         return jarsPath;
     }
 
-    public void setJarsPath(String jarsPath) {
+    public void setJarsPath(String jarsPath) {//testado 
         this.jarsPath = jarsPath;
     }
 
-    public void updateConnectionParams(String ooPath, String execPath, String jarsPath) {
+    public void updateConnectionParams(String ooPath, String execPath, String jarsPath) {//testado
         setInstallationPath(ooPath);
         setExecutablePath(execPath);
         setJarsPath(jarsPath);
     }
 
-    public void clearConnectionSettings() {
+    public void clearConnectionSettings() {//testado
         this.installationPath = null;
         this.executablePath = null;
         this.jarsPath = null;
